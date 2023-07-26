12c6879ce386632a7e207bc05f25a470c5939278
Mykola <nikolaykrasniuk@gmail.com>
2023-07-26 20:08:54 +0300

    created structure project

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoList from '../app/components/todoList/TodoList';
+const App = () => {
+	return (
+		<div>
+			<TodoList />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/app/components/todoList/TodoList.js
@@ -0,0 +1,12 @@
+import TodoItem from '../todoItem/TodoItem';
+import TodoFilters from '../todoFilters/TodoFilters';
+const TodoList = () => {
+
+    return (
+      <div>
+            
+      </div>
+    );
+  };
+  
+export default TodoList;
